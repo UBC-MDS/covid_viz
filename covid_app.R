5e34e1a3886aefd5428ebe8c34379ca0bcb39374
Ashwin Babu <ash2507@student.ubc.ca>
2023-02-16 13:19:01 -0800

    Added UI for the R shiny app

--- covid_app.R
+++ covid_app.R
@@ -1,3 +1,50 @@
 library(shiny)
 library(shinythemes)
 library(showtext)
+
+my_theme <- bslib::bs_theme(bootswatch = "darkly",base_font = font_add_google("Righteous"))
+thematic::thematic_shiny(font = "auto")
+
+ui <- shinyUI(fluidPage(
+  theme = my_theme,
+  titlePanel("Covid-19 Tracker Dashboard"),
+  
+  fluidRow(
+    column(2,
+           selectInput(
+             inputId = "countrydropdown",
+             label = "Country Dropdown",
+             choices = list(1,2,3,4,5,6,7,8)
+           ),
+           sliderInput(
+             inputId = "gdpslider",
+             label = "GDP Slider",
+             min = 0,
+             max = 10,
+             value = 5
+           ),
+           sliderInput(
+             inputId = "popdenslider",
+             label = "Population Density slider",
+             min = 0,
+             max = 10,
+             value = 5
+           ),
+           style="overflow-x: scroll; overflow-y: scroll"),
+    column(6,
+           plotOutput("travelindexPlot"),
+           sliderInput(
+             inputId = "dateslider",
+             label = "Date Slider",
+             min = 0,
+             max = 100,
+             value =  c(40, 60),
+             width = '100%'),
+           ),
+    column(4,
+           plotOutput("casesPlot", width = "100%"),
+           plotOutput("deathPlot"),
+           plotOutput("vaccinePlot"),
+           style="overflow-x: scroll; overflow-y: scroll")
+  )
+))
