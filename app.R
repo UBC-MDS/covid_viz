f25ba91004cbf2e41a8f3aee1f8e6eb841e1c416
Jenit Jain <jenitjai@student.ubc.ca>
2023-03-02 15:54:45 -0800

    Add colors to all countries

--- app.R
+++ app.R
@@ -4,26 +4,65 @@ library(leaflet)
 library(jsonlite)
 library(showtext)
 library(readr)
+library(RColorBrewer)
+library(lubridate)
+library(dplyr)
 
 my_theme <-
   bslib::bs_theme(bootswatch = "darkly", base_font = font_add_google("Righteous"))
 thematic::thematic_shiny(font = "auto")
 data <- readr::read_csv("data/raw/owid-covid-data.csv")
-data$date <- as.Date(data$date, format = "%Y/%m/%d")
-geojson <- readLines("https://raw.githubusercontent.com/datasets/geo-countries/master/data/countries.geojson", warn = FALSE) |>
+data$date <- ymd(data$date)
+geojson <-
+  readLines(
+    "https://raw.githubusercontent.com/datasets/geo-countries/master/data/countries.geojson",
+    warn = FALSE
+  ) |>
   paste(collapse = "\n") |>
   jsonlite::fromJSON(simplifyVector = FALSE)
 # Finding values to populate the data input widgets
 countries <- c("Worldwide")
-for(feature_num in seq(length(geojson[[2]]))){
+for (feature_num in seq(length(geojson[[2]]))) {
   countries <- append(countries, geojson[[2]][[feature_num]]$properties$ADMIN)
 }
+
+colors <- colorBin("RdYlGn", 
+                   domain = list(0,100), 
+                   bins = seq(0,100,10))
 min_gdp <- min(data$gdp_per_capita, na.rm = TRUE)
 max_gdp <- max(data$gdp_per_capita, na.rm = TRUE)
 max_pop_density <- max(data$population_density, na.rm = TRUE)
 min_pop_density <- min(data$population_density, na.rm = TRUE)
 min_date <- min(data$date)
 max_date <- max(data$date)
+
+get_color <- function(country,
+                      start_date,
+                      end_date) {
+  filtered_df <- data |>
+                  filter(location == country) |> 
+                  filter(date %within% interval(start = ymd(start_date), end = ymd(end_date))) |> 
+                  filter(!is.na(stringency_index))
+  
+  if(nrow(filtered_df) == 0){
+    value <- 0
+  }
+  else{
+    value <-filtered_df[["stringency_index"]][[nrow(filtered_df)]]
+    if(is.null(value)){
+      value <- 0
+    }
+  }
+  colors(100-value)
+}
+
+
+get_countries <- function(popden, gdp){
+  data |> 
+    filter(population_density <= popden & gdp_per_capita <= gdp) |>
+    select(location) |>
+    unique() |> pull()
+}
 
 ui <- shinyUI(fluidPage(
   theme = my_theme,
@@ -77,38 +116,61 @@ ui <- shinyUI(fluidPage(
 
 server <- function(input, output) {
   thematic::thematic_shiny()
-  
-  
+
   output$travelindexPlot <- renderLeaflet({
-    worldwide <- TRUE
-    lng <- 0
-    lat <- 0
-    zoom <- 2
-    # if(input$countrydropdown != "Worldwide"){
-    #   worldwide <- FALSE
-    #   for(num in seq(length(countries)-1)){
-    #     if(countries[num] == input$countrydropdown){
-    #           lng <- 93.85532
-    #           zoom <- 5
-    #           lat<-7.214179
-    #           break
-    #     }
-    #   }
-    # }
-
-    # geojson$style = list(
-    #   weight = 1,
-    #   color = "#555555",
-    #   opacity = 1,
-    #   fillOpacity = 0.8
-    # )
-
-    leaflet() |>
-      setView(lng = lng, lat = lat, zoom = zoom) |>
-      addTiles() |>
-      addGeoJSON(geojson)
-
-    })
+    map <- leaflet() |>
+      setView(lng = 0,
+              lat = 0,
+              zoom = 1.5) |> 
+      addTiles()
+    
+    if (input$countrydropdown != "Worldwide") {
+      for (num in seq(2, length(countries))) {
+        if (countries[num] == input$countrydropdown) {
+          break
+        }
+      }
+      country_json <- list(type = geojson[[1]],
+                           features = list(geojson[[2]][[num - 1]]))
+      country_json$style = list(
+        weight = 1,
+        fill = TRUE,
+        fillColor = get_color(countries[num],
+                              input$dateslider[[1]],
+                              input$dateslider[[2]]),
+        color = "black",
+        opacity = 0.8,
+        fillOpacity = 0.8
+      )
+      map <- map |> addGeoJSON(geojson = country_json) 
+    }
+    else{
+      filtered_countries <- get_countries(input$popdenslider, input$gdpslider)
+      all_features <- list()
+      final_countries = list()
+      for(country in filtered_countries){
+        for (num in seq(2, length(countries))) {
+          if (countries[num] == country) {
+            country_json <- list(type = geojson[[1]],
+                                 features = list(geojson[[2]][[num - 1]]))
+            country_json$style = list(
+              weight = 1,
+              fill = TRUE,
+              fillColor = get_color(country,
+                                    input$dateslider[[1]],
+                                    input$dateslider[[2]]),
+              color = "black",
+              opacity = 0.8,
+              fillOpacity = 0.8
+            )
+            map <- map |> addGeoJSON(geojson = country_json)
+            break
+          }
+        }
+      }
+    }
+    map
+  })
   
   output$casesPlot <- renderPlot({
     x    <- faithful[, 2]
