9c5ea8c711c2a78ff7f6559ee37efe87551ea673
lennonay <lennon.auyeung@me.com>
2023-03-16 21:44:26 -0700

    added tests

--- tests/testthat/test-shinytest2.R
+++ tests/testthat/test-shinytest2.R
@@ -1,12 +1,32 @@
 library(shinytest2)
 
-test_that("{shinytest2} recording: covid_viz_slider", {
+test_that("{shinytest2} recording: gdpslider", {
   app <- AppDriver$new(variant = platform_variant(), name = "covid_viz", height = 782, 
       width = 1211)
   app$set_inputs(gdpslider = c(662, 116935.6))
-  app$set_inputs(popdenslider = c(662, 20546.766))
+  app$expect_values()
+  app$expect_screenshot()
+})
+
+test_that("{shinytest2} recording: popdenslider", {
+  app <- AppDriver$new(variant = platform_variant(), name = "covid_viz", height = 782, 
+      width = 1211)
   app$set_inputs(popdenslider = c(0.137, 20546.766))
+  app$expect_values()
+  app$expect_screenshot()
+})
+
+test_that("{shinytest2} recording: dateslider", {
+  app <- AppDriver$new(variant = platform_variant(), name = "covid_viz", height = 782, 
+      width = 1211)
   app$set_inputs(dateslider = c("2020-08-12", "2023-02-12"))
+  app$expect_values()
+  app$expect_screenshot()
+})
+
+test_that("{shinytest2} recording: countrydropdown", {
+  app <- AppDriver$new(variant = platform_variant(), name = "covid_viz", height = 782, 
+      width = 1211)
   app$set_inputs(countrydropdown = "Hong Kong S.A.R.")
   app$expect_values()
   app$expect_screenshot()
