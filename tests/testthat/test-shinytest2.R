2aac7440008eb6f47183ccc39ca87b5a55627ba9
BruceUBC <wu.shiliang@outlook.com>
2023-03-17 21:32:42 -0700

    solve merge conflict

--- tests/testthat/test-shinytest2.R
+++ tests/testthat/test-shinytest2.R
@@ -15,6 +15,17 @@ test_that("{shinytest2} recording: country_date_dropdown", {
   app$expect_values()
 })
 
+test_that("{shinytest2} recording: GDP_slider", {
+  app <- AppDriver$new(name = "GDP_slider", height = 882, width = 1619)
+  app$set_inputs(gdpslider = c(662, 55661))
+  app$expect_values()
+})
+
+test_that("{shinytest2} recording: covid_viz_test_1", {
+  app <- AppDriver$new(name = "covid_viz_test_1", height = 944, width = 1619)
+  app$set_inputs(popdenslider = c(0.137, 1000))
+  app$expect_values()
+})
 
 
 
@@ -28,24 +39,12 @@ test_that("{shinytest2} recording: argentina", {
 
 
 
-
-test_that("{shinytest2} recording: visualization test", {
+test_that("{shinytest2} recording: covid_viz", {
   app <- AppDriver$new(variant = platform_variant(), name = "covid_viz", height = 963, 
       width = 1407)
   app$set_inputs(gdpslider = c(662, 74661))
   app$set_inputs(popdenslider = c(200, 15400))
   app$set_inputs(dateslider = c("2021-07-30", "2023-02-12"))
   app$expect_screenshot()
-
-test_that("{shinytest2} recording: GDP_slider", {
-  app <- AppDriver$new(name = "GDP_slider", height = 882, width = 1619)
-  app$set_inputs(gdpslider = c(662, 55661))
-  app$expect_values()
 })
 
-test_that("{shinytest2} recording: covid_viz_test_1", {
-  app <- AppDriver$new(name = "covid_viz_test_1", height = 944, width = 1619)
-  app$set_inputs(popdenslider = c(0.137, 1000))
-  app$expect_values()
-
-})
