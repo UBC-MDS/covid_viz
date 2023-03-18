971157a4f7d8d781cc7096f3b28396664b797c89
lennonay <lennon.auyeung@me.com>
2023-03-17 18:24:58 -0700

    add third test

--- tests/testthat/test-shinytest2.R
+++ tests/testthat/test-shinytest2.R
@@ -1,14 +1,5 @@
 library(shinytest2)
 
-test_that("{shinytest2} recording: GDP_slider", {
-  app <- AppDriver$new(variant = platform_variant(), name = "GDP_slider", height = 782, 
-      width = 1211)
-  app$set_window_size(width = 1619, height = 882)
-  app$set_inputs(gdpslider = c(662, 55661))
-  app$expect_values()
-})
-
-
 test_that("{shinytest2} recording: popdens_date_slider", {
   app <- AppDriver$new(name = "popdens_date_slider", height = 882, width = 1619)
   app$set_inputs(popdenslider = c(0.137, 1000))
@@ -21,5 +12,11 @@ test_that("{shinytest2} recording: country_date_dropdown", {
   app <- AppDriver$new(name = "country_date_dropdown", height = 882, width = 1619)
   app$set_inputs(countrydropdown = "Afghanistan")
   app$set_inputs(dateslider = c("2020-01-01", "2021-07-25"))
+  app$expect_values()
+})
+
+test_that("{shinytest2} recording: GDP_slider", {
+  app <- AppDriver$new(name = "GDP_slider", height = 882, width = 1619)
+  app$set_inputs(gdpslider = c(662, 55661))
   app$expect_values()
 })
